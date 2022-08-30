5fd28c0ecb4ffd39588ff40310423614bfedd76b
Austin Dewey <deweya964@gmail.com>
2022-08-29 22:49:30 -0400

    works for 'racecar'

--- ch9-2/ch9-2.asm
+++ ch9-2/ch9-2.asm
@@ -13,13 +13,7 @@ SYS_exit    equ     0x2000001
 
 ; Variables
 string      db      "racecar", 0        ; The string that we will check if it's a palindrome
-isPdrome    db      0
-
-; ***************************
-
-section     .bss
-
-compare     resb    50                  ; The comparison string, to help determine if the original string is a palindrome
+isPdrome    db      1
 
 ; ***************************
 
@@ -28,46 +22,35 @@ global _start
 _start:
 
     lea     r8, [string]
-    mov     r9, 0                       ; displacement for string
-    lea     r10, [compare]
     mov     r11, 0                      ; displacement for compare
     mov     rcx, 0                      ; length of string
 
 pushStart:
 
-    mov     al, [r8 + r9]
+    mov     al, [r8 + rcx]
     push    rax
-    inc     rcx
 
-    cmp     rax, 0
+    cmp     rax, 0                      ; If we reached the null terminating char, then jump out of the loop
     je      popStart
 
-    inc     r9
+    inc     rcx
     jmp     pushStart
 
 popStart:
 
     pop     rax
-    cmp     rax, 0                      ; We don't want to push the null terminating character to "compare"
-    jne     addToCompare
-    loop    popStart
+    mov     bl, byte [r8 + rcx]
+    cmp     al, bl
+    jne     notPdrome
+    
+    cmp     rcx, 0
+    je      exit
+    dec     rcx
+    jmp     popStart
 
-addToCompare:
+notPdrome:
 
-    mov     [r10 + r11], rax
-    inc     r11
-    loop    popStart
-
-determinePdrome:
-
-    mov     rax, [r8]
-    cmp     rax, [compare]
-    je      setPdrome
-    jne     exit
-
-setPdrome:
-
-    mov     byte [isPdrome], 1
+    mov     byte [isPdrome], 0
 
 exit:
     mov     rax, SYS_exit
