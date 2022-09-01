247a7b369ac6ad5beb850ef3176110596068d484
Austin Dewey <deweya964@gmail.com>
2022-09-01 12:15:56 -0400

    changes to read string correctly

--- ch9-2/ch9-2.asm
+++ ch9-2/ch9-2.asm
@@ -22,30 +22,39 @@ global _start
 _start:
 
     lea     r8, [string]
-    mov     r11, 0                      ; displacement for compare
-    mov     rcx, 0                      ; length of string
+    mov     rcx, 0
 
 pushStart:
 
     mov     al, [r8 + rcx]
-    push    rax
+    push    rax                         ; Push character to stack
 
     cmp     rax, 0                      ; If we reached the null terminating char, then jump out of the loop
-    je      popStart
+    je      resetCounter
 
     inc     rcx
     jmp     pushStart
+
+resetCounter:
+
+    pop     rax                         ; Pop the null terminating char, since we won't use it
+    mov     rdx, rcx                    ; rdx is the len of the string (excluding null-terminating char)
+    mov     rcx, 0                      ; Reset the counter so we can read the original string front-to-back
 
 popStart:
 
     pop     rax
     mov     bl, byte [r8 + rcx]
     cmp     al, bl
-    jne     notPdrome
+    jne     notPdrome                   ; As soon as a char doesn't match, short circuit and exit
     
     cmp     rcx, 0
     je      exit
-    dec     rcx
+
+    inc     rcx
+    cmp     rcx, rdx                    ; If we reached the length of the original string, then exit
+    je      exit
+
     jmp     popStart
 
 notPdrome:
