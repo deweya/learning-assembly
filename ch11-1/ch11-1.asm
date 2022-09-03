d10152b02d2cc227468708122d7c6d6e9e4f24f9
Austin Dewey <deweya964@gmail.com>
2022-09-03 12:13:27 -0400

    min finished

--- ch11-1/ch11-1.asm
+++ ch11-1/ch11-1.asm
@@ -1,5 +1,8 @@
 ; Chapter 11, exercise 1
-; Implement a macro for finding the average of a list of numbers
+; Implement macros for finding (against a list of integers):
+;   * average
+;   * min
+;   * max
 
 default rel
 
@@ -25,6 +28,26 @@ default rel
     mov     [%1], ax
 %endmacro
 
+; -----
+; Find the minimum in a list of numbers
+%macro  fMin    2
+    lea     r8, [%2]
+    mov     cl, [len]
+    mov     r9, 0
+%%loopMin:
+    mov     ax, [r8 + r9*2]
+    cmp     [%1], ax
+    jg      %%setMin
+%%incLoop:
+    inc     r9
+    loop    %%loopMin
+    jmp     %%done
+%%setMin:
+    mov     [%1], ax
+    jmp     %%incLoop
+%%done:
+%endmacro
+
 ; *********************
 
 section     .data
@@ -35,6 +58,8 @@ SYS_exit    equ     0x2000001
 lst         dw      100, 750, 22, 0, 555
 len         db      5
 avg         dw      0
+min         dw      0x7fff
+max         dw      0
 
 ; *********************
 
@@ -43,6 +68,7 @@ global _start
 _start:
 
     cAvg    avg, lst
+    fMin    min, lst
 
 exit:
     mov     rax, SYS_exit
